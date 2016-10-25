1599a61437057318f6f43df127377e34656f1579
Joe Attardi <jattardi@constantcontact.com>
2016-10-25 16:20:22 -0400

    refactor cookie handling code

--- src/cookies.js
+++ src/cookies.js
@@ -4,15 +4,17 @@ const _ = require('lodash');
 const request = require('request');
 const debug = require('debug')('cookies');
 
+function addCookie(jar, cookie, url) {
+  debug(`Adding cookie: ${cookie}`);
+  jar.setCookie(request.cookie(cookie), url);
+}
+
 exports.processCookies = function handleCookies(cookieParams, options) {
   if (cookieParams) {
     const jar = request.jar();
-
     options.jar = jar;
+
     const cookies = _.isArray(cookieParams) ? cookieParams : [ cookieParams ];
-    cookies.forEach(cookie => {
-      debug(`Handling cookie ${cookie}`);
-      jar.setCookie(request.cookie(cookie), options.url); 
-    });
+    cookies.forEach(cookie => addCookie(jar, cookie, options.url));
   }
 };
